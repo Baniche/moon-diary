cf3f95ef880c625a6b586a7337ef4c2573cf210f
Baniche <dinobs123421@gmail.com>
2025-05-18 17:21:04 +0300

    add start screen and a litte js code

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 /* Логіка сторінки */
+
+let start_screen = document.querySelector('.start-scr')
+let start_btn = document.querySelector('.start-btn')
+
+start_btn.addEventListener('click', function() {
+    start_screen.style.display = 'none'
+})
+
