1781a6b416531e5843aaf32ce3db75433e3890ca
Baniche <dinobs123421@gmail.com>
2025-05-30 21:48:12 +0300

    add localStorage

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ start_btn.addEventListener('click', function() {
     start_screen.style.display = 'none'
 })
 
+
+let selectedEmoji = null;
+
+document.querySelectorAll('.emoji').forEach((emoji, index) => {
+    emoji.addEventListener('click', () => {
+        selectedEmoji = `emoji-${index + 1}`;
+        
+       
+        document.querySelectorAll('.emoji').forEach(e => e.classList.remove('selected'));
+        emoji.classList.add('selected');
+    });
+});
+
+document.querySelector('.sumbit-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const comment = document.querySelector('.input-comment').value;
+
+    const data = {
+        emoji: selectedEmoji,
+        comment: comment
+    };
+   
+    localStorage.setItem('moodEntry', JSON.stringify(data));
+  
+    console.log("Збережено:", JSON.stringify(data, null, 2));
+});
