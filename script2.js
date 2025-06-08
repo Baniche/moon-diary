8a1a87b33c57fabf7b13f08bffede2ee02ceda99
Baniche <dinobs123421@gmail.com>
2025-06-08 20:11:50 +0300

    7

--- script2.js
+++ script2.js
@@ -83,7 +83,7 @@ window.addEventListener("DOMContentLoaded", () => {
     Object.entries(diary).forEach(([iso, data]) => {
       const cell = document.querySelector(`[data-date='${iso}']`);
       if (cell) {
-        const emoji = typeof data === "string" ? data : data.mood;
+        const emoji = typeof data === "string" ? data : data.mood || "❌";
         cell.textContent = emoji;
         cell.classList.add("has-emoji");
       }
